93ab35f983780dafebe63fb641c953f723ce4367
ghiboz <info@ghiboz.com>
2021-06-24 14:08:57 +0200

    clean code

--- Ocr/Program.cs
+++ Ocr/Program.cs
@@ -15,14 +15,6 @@ namespace Ocr
         [STAThread]
         static void Main(string[] args)
         {
-            /*
-            var testImagePath = $@"C:\Users\ghiboz\Documents\ShareX\Screenshots\2021-06\firefox_2021-06-24_09-25-17.png";
-            if (args.Length > 0)
-            {
-                testImagePath = args[0];
-            }
-            */
-
             var imgUrl = Clipboard.GetText();
             if (string.IsNullOrEmpty(imgUrl))
             {
@@ -36,15 +28,11 @@ namespace Ocr
             using (WebClient client = new WebClient())
             {
                 client.DownloadFile(new Uri(imgUrl), testImagePath);
-                // OR 
-                //client.DownloadFileAsync(new Uri(url), @"c:\temp\image35.png");
             }
 
             try
             {
                 var logger = new FormattedConsoleLogger();
-                var resultPrinter = new ResultPrinter(logger);
-                // using (var engine = new TesseractEngine(@"C:\Temp\Tesseract-OCR\tessdata", "eng", EngineMode.Default))
                 using (var engine = new TesseractEngine(@".\tessdata", "eng", EngineMode.Default))
                 {
                     using (var img = Pix.LoadFromFile(testImagePath))
@@ -65,38 +53,12 @@ namespace Ocr
             }
             catch (Exception e)
             {
-                //Trace.TraceError(e.ToString());
                 Console.WriteLine("Unexpected Error: " + e.Message);
                 Console.WriteLine("Details: ");
                 Console.WriteLine(e.ToString());
             }
             Console.Write("Press any key to continue . . . ");
             Console.ReadKey(true);
-        }
-    }
-
-    class ResultPrinter
-    {
-        readonly FormattedConsoleLogger logger;
-
-        public ResultPrinter(FormattedConsoleLogger logger)
-        {
-            this.logger = logger;
-        }
-
-        public void Print(ResultIterator iter)
-        {
-            logger.Log("Is beginning of block: {0}", iter.IsAtBeginningOf(PageIteratorLevel.Block));
-            logger.Log("Is beginning of para: {0}", iter.IsAtBeginningOf(PageIteratorLevel.Para));
-            logger.Log("Is beginning of text line: {0}", iter.IsAtBeginningOf(PageIteratorLevel.TextLine));
-            logger.Log("Is beginning of word: {0}", iter.IsAtBeginningOf(PageIteratorLevel.Word));
-            logger.Log("Is beginning of symbol: {0}", iter.IsAtBeginningOf(PageIteratorLevel.Symbol));
-
-            logger.Log("Block text: \"{0}\"", iter.GetText(PageIteratorLevel.Block));
-            logger.Log("Para text: \"{0}\"", iter.GetText(PageIteratorLevel.Para));
-            logger.Log("TextLine text: \"{0}\"", iter.GetText(PageIteratorLevel.TextLine));
-            logger.Log("Word text: \"{0}\"", iter.GetText(PageIteratorLevel.Word));
-            logger.Log("Symbol text: \"{0}\"", iter.GetText(PageIteratorLevel.Symbol));
         }
     }
 }
